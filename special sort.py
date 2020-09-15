4c2aebc7342a4944a48ef9b9d3466b237c84d46e
Robert <robert.mullaney@rocky.edu>
2020-09-15 11:38:14 -0600

    Update special sort.py

--- special sort.py
+++ special sort.py
@@ -9,7 +9,3 @@ def specialSort(toSort):
             odd.append(item)
         count +=1
     return even+odd
-lst=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(specialSort(lst))
-lst2= ['this', 'is', 'special', 'right', 'sort']
-print(specialSort(lst2))
