7510df5fbc1075c5860c2b5dc0f677c3e62e8bc5
Robert <robert.mullaney@rocky.edu>
2020-09-22 11:12:46 -0600

    Update calculator.py

--- hw3/calculator.py
+++ hw3/calculator.py
@@ -1,12 +1,9 @@
 def addition(value1, value2):
     return value1+value2
-
 def subtraction(value1, value2):
     return value1-value2
-
 def multiplication(value1, value2):
     return value1*value2
-
 def division(value1, value2):
     if value2==0:
         return 0
@@ -15,7 +12,26 @@ def division(value1, value2):
     else:
         return str(value1//value2)+','+str(value1%value2)
 
-print(addition(2, 3))
-print(subtraction(9, 3))
-print(multiplication(3, 3))
-print(division(10, 8))
+
+
+while True:
+    x = input('What would you like to do? ')
+    
+    if x == 'done':
+        print('Thanks for using my calculator')
+        break
+
+    y = int(input('Enter value1 '))
+    z = int(input('Enter value2 '))
+
+    if x == 'add':
+        print(addition(y,z))
+
+    if x == 'subtract':
+        print(subtraction(y, z))
+
+    if x == 'multiply':
+        print(multiplication(y, z))
+
+    if x == 'divide':
+        print(division(y, z))
